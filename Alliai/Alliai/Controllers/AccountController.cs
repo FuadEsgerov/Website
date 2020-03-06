a70ee1e2d4cc83f4e9d9f270c10659d3ba1e178e
FuadEsgerov <56359250+FuadEsgerov@users.noreply.github.com>
2020-03-06 19:11:08 +0400

    Controllers and Views

---
+++ Alliai/Alliai/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alliai.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Confirm()
+        {
+            return View();
+        }
+    }
+}
---
+++ Alliai/Alliai/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alliai.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult LeaveReview()
+        {
+            return View();
+        }
+    }
+}
